6b7097a7ec5fa0e30762f09b97a72d71ca8ad463
Andrei Pfeiffer <andrei@upsidedown.ro>
2021-01-07 17:18:56 +0200

    Use TS enum for dynamic styles

--- components/button.tsx
+++ components/button.tsx
@@ -1,8 +1,14 @@
 import styles from "./button.module.css";
 
+enum Color {
+  blue = "dodgerblue",
+  yellow = "#e3b307",
+  green = "#57b004",
+}
+
 type Props = {
   children: string;
-  color?: string;
+  color?: keyof typeof Color;
 };
 
 export function Button(props: Props) {
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <main>
       <Button>Default color</Button>
-      <Button color="#e3b307">Yellow color</Button>
-      <Button color="#57b004">Green color</Button>
+      <Button color="yellow">Yellow color</Button>
+      <Button color="green">Green color</Button>
     </main>
   );
 }
