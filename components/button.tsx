6c033ddc4d763d7e0ea395f54356575a746d15bd
Andrei Pfeiffer <andrei@upsidedown.ro>
2021-01-07 17:47:09 +0200

    Use styled-jsx

--- components/button.tsx
+++ components/button.tsx
@@ -1,4 +1,4 @@
-import styles from "./button.module.css";
+import { useEffect, useState } from "react";
 
 enum Color {
   blue = "dodgerblue",
@@ -12,11 +12,83 @@ type Props = {
 };
 
 export function Button(props: Props) {
-  const { children, color = "dodgerblue" } = props;
+  const { children, color = "blue" } = props;
+
+  let [isDesktop, setIsDesktop] = useState(false);
+
+  // media queries not supported our of the box, so we need to do this shit
+  useEffect(() => {
+    const isDesktop = window.matchMedia("only screen and (min-width: 640px)");
+    setIsDesktop(isDesktop.matches);
+
+    isDesktop.addEventListener("change", updateIsDesktop);
+
+    function updateIsDesktop(e: MediaQueryListEvent) {
+      setIsDesktop(e.matches);
+    }
+
+    return () => {
+      isDesktop.removeEventListener("change", updateIsDesktop);
+    };
+  }, []);
 
   return (
-    <button className={styles.button} style={{ color }}>
+    <button>
       <span>{children}</span>
+
+      <style jsx>
+        {`
+          button {
+            border: 0;
+            height: 3em;
+            padding: 0 2em;
+            border-radius: 1.5em;
+            cursor: pointer;
+            font-weight: bold;
+            width: 100vw;
+            display: block;
+            outline: 0;
+            background-color: currentColor;
+            margin: 1em auto;
+          }
+
+          button span {
+            color: white;
+          }
+
+          button:hover {
+            animation-name: button-animation;
+            animation-fill-mode: forwards;
+            animation-duration: 0.5s;
+          }
+
+          @media only screen and (min-width: 640px) {
+            .button {
+            }
+          }
+
+          @keyframes button-animation {
+            from {
+              transform: translateY(0);
+              box-shadow: none;
+            }
+            to {
+              transform: translateY(-0.5em);
+              box-shadow: 0 0.5em 0 0 #ddd;
+            }
+          }
+        `}
+      </style>
+
+      <style jsx>
+        {`
+          // dynamic styles
+          button {
+            color: ${Color[color]};
+            width: ${isDesktop === true ? "100%" : "100vw"};
+          }
+        `}
+      </style>
     </button>
   );
 }
