c6fed6350cf1a318e9db5bfa7b3f5a6c98a05870
Andrei Pfeiffer <andrei@upsidedown.ro>
2021-01-18 11:11:07 +0200

    Add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
-const withTreat = require("next-treat")(/* Extra TreatPlugin options */);
+const withPlugins = require("next-compose-plugins");
 
-module.exports = withTreat(/* Additional Next.js configuration */);
+const withTreat = require("next-treat")(/* Extra TreatPlugin options */);
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withPlugins([
+  [withBundleAnalyzer],
+  [withTreat],
+  // your other plugins here
+]);
