1c842162617fe994f3cf827ce03d30825a05317e
Andrei Pfeiffer <andrei@upsidedown.ro>
2021-01-28 19:22:28 +0200

    Remove unneeded code

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 const withPlugins = require("next-compose-plugins");
 
-const withTreat = require("next-treat")(/* Extra TreatPlugin options */);
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
 module.exports = withPlugins([
   [withBundleAnalyzer],
-  [withTreat],
   // your other plugins here
 ]);
