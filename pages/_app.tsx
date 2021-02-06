38478751348b1712a79fac53a024f8dd7158f371
Andrei Pfeiffer <andrei@upsidedown.ro>
2021-02-06 09:43:45 +0200

    Use global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import App from "next/app";
-
-import "../styles/globals.css";
+import injectSheet from "react-jss";
 
 export class MyApp extends App {
   componentDidMount() {
@@ -12,4 +11,32 @@ export class MyApp extends App {
   }
 }
 
-export default MyApp;
+const style = {
+  "@global": {
+    ["html, body"]: {
+      padding: 0,
+      margin: 0,
+      minHeight: "100vh",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      textAlign: "center",
+    },
+    ["html, body, button, input"]: {
+      fontFamily: `-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif`,
+      fontSize: "16px",
+    },
+    ["a:link, a:visited"]: {
+      color: "dodgerblue",
+      textDecoration: "none",
+    },
+    ["a:hover"]: {
+      textDecoration: "underline",
+    },
+    ["*"]: {
+      boxSizing: "border-box",
+    },
+  },
+};
+
+export default injectSheet(style)(MyApp);
