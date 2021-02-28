9a32860d88a4b6604f8ed675b6b453dc6a8e75cf
Andrei Pfeiffer <andrei@upsidedown.ro>
2021-02-28 13:09:34 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,9 +3,4 @@ const withPlugins = require("next-compose-plugins");
 const withTM = require("next-transpile-modules")(["style9"]);
 const withStyle9 = require("style9/next");
 
-module.exports = withPlugins([
-  [withStyle9()(withTM())],
-  [
-    // your other plugins here, or next config
-  ],
-]);
+module.exports = withPlugins([[withStyle9()(withTM())]]);
