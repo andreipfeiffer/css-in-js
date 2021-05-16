aa4c959c7172869721687983dd89db75a9efb163
hookydev <dapetrov0@gmail.com>
2021-05-16 13:04:53 +0300

    Added linaria example

---
+++ next-linaria.js
@@ -0,0 +1,61 @@
+const LINARIA_EXTENSION = '.linaria.module.css'
+const DEV = process.env.NODE_ENV !== 'production'
+
+function traverse(rules) {
+  for (let rule of rules) {
+    if (typeof rule.loader === 'string' && rule.loader.includes('css-loader')) {
+      if (
+        rule.options &&
+        rule.options.modules &&
+        typeof rule.options.modules.getLocalIdent === 'function'
+      ) {
+        let nextGetLocalIdent = rule.options.modules.getLocalIdent
+        rule.options.modules.getLocalIdent = (
+          context,
+          _,
+          exportName,
+          options,
+        ) => {
+          if (context.resourcePath.includes(LINARIA_EXTENSION)) {
+            return exportName
+          }
+          return nextGetLocalIdent(context, _, exportName, options)
+        }
+      }
+    }
+    if (typeof rule.use === 'object') {
+      traverse(Array.isArray(rule.use) ? rule.use : [rule.use])
+    }
+    if (Array.isArray(rule.oneOf)) {
+      traverse(rule.oneOf)
+    }
+  }
+}
+
+module.exports = (nextConfig = {}) => {
+  return {
+    ...nextConfig,
+    webpack(config, options) {
+      traverse(config.module.rules)
+      config.module.rules.push({
+        test: /\.(tsx|ts|js|mjs|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: require.resolve('@linaria/webpack-loader'),
+            options: {
+              sourceMap: DEV,
+              ...(nextConfig.linaria || {}),
+              extension: LINARIA_EXTENSION,
+            },
+          },
+        ],
+      })
+
+      if (typeof nextConfig.webpack === 'function') {
+        return nextConfig.webpack(config, options)
+      }
+      return config
+    },
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,29 @@
+import { styled } from '@linaria/react';
+
+const Box = styled.div`
+  margin-top: 40px;
+  margin-left: 40px;
+  height: 200px;
+  width: 200px;
+  background-color: tomato;
+  animation: spin 2s linear infinite;
+
+  @keyframes spin {
+    from {
+      transform: rotate(0deg);
+    }
+
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`
+
 export default function Page() {
   return (
     <main>
       <h1>Index page</h1>
+      <Box>Hello, Linaria!</Box>
     </main>
   );
 }
